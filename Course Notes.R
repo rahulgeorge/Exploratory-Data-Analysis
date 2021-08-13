e7fbc280ff13ab36817dcb468ed611d9ea2faa65
Rahul George <rahulgeorge.secure@gmail.com>
2021-08-13 16:18:55 +0530

    Completed Week 2

--- Course Notes.R
+++ Course Notes.R
@@ -192,4 +192,175 @@ dev.copy(png, file = "geysorplot.png") #Copy my plot to png
 dev.copy2pdf(file = "geysor.pdf") #Direct export to PDF
 dev.off()
 
-#Swirl GGPLOT
+
+#Lattice Plotting System
+library(lattice)
+library(datasets)
+
+xyplot(Ozone ~ Wind, data = airquality) #Scatterplot
+xyplot(Ozone ~ Wind | Month, data = airquality, layout = c(5,1)) #Creates plot across all month levels
+p <- xyplot(Ozone ~ Wind, data = airquality)
+print(p) #Lattice system returns an objet of class trellis. It can be stored and printed later. But better to generate and print
+
+#Lattice Panel Functions
+set.seed(10)
+x <- rnorm(100)
+f <- rep(0:1, each =50)
+y <- x + f - f * x + rnorm(100, sd = 0.5)
+f <- factor(f, labels = c("Group 1", "Group 2"))
+xyplot(y ~ x | f, layout = c(2,1)) #Plot with panels
+
+xyplot(y ~ x |f, panel = function(x, y, ...) {
+        panel.xyplot(x, y, ...) #Call the default panel function for xyplot
+        panel.abline(h = median(y), lty = 2) #Adding horizontal line at median
+        panel.lmline(x, y, col = 2) #Adding a regression line
+})
+
+
+
+#GGPLOT2 Package
+library(ggplot2)
+str(mpg)
+qplot(displ, hwy, data = mpg) #Basic plot with quickplot function
+qplot(displ, hwy, data = mpg, color = drv) #Legend prepared and graph colored as per dev variable
+qplot(displ, hwy, data = mpg, geom = c("point","smooth"), color = drv) #Adding a geometric object which maps all the points and is smooth 
+qplot(hwy, data = mpg, fill = drv) #Histogram by specifying just one varibale. Colored by drv
+
+qplot(displ, hwy, data = mpg, facets = .~drv) #Similar to panels. Split by drv where facets take 2 argument. Left of ~ is rows variable and right if columns
+qplot(hwy, data = mpg, facets = drv~., binwidth = 2) #Set the width of each bin
+
+load(file = "./data/maacs.rda")
+str(maacs)
+qplot(log(eno), data = maacs, fill = mopos) 
+qplot(log(eno), data = maacs, geom = "density")
+qplot(log(eno), data = maacs, geom = "density", color = mopos)
+
+qplot(log(pm25),log(eno),data=maacs, shape = mopos, color = mopos)
+qplot(log(pm25),log(eno),data=maacs, shape = mopos, color = mopos) + geom_smooth(method = "lm")
+qplot(log(pm25), log(eno), data = maacs, facets = .~mopos) + geom_smooth(method = "lm")
+
+##GGPLOT2 Components
+## 1. Aesthetic mappings - data to color and size
+## 2. Geoms - lines, points, shapes
+## 3. Facets - conditional plot
+## 4. Stats - Statistical transformation such as quantile, smoothing
+## 5. Scales - aesthetic map scale, legend (ex. male = red, female = blue)
+## 6. Coordinate System
+
+str(maacs)
+qplot(log(pm25), )
+
+#Using below code to generate dummy maacs data
+                bootstrapMissingVars <- function(){
+                      
+                        #some variables have been removed for privacy reasons so
+                        #I'm gonna "bootstrap" a fake variables in their place,
+                        #based off the graphs I can see
+                        
+                        library(dplyr)
+                        #After some quick arthmetic, it seems logpm25 
+                        #used log base 10
+                        maacs <- mutate(maacs, logpm25 = log(pm25, 10))
+                        
+                        #Generating NocturnalSympt
+                        # No values below 0 were shown so I figured I'd drop these
+                        # And get rid of any NA values at this point
+                        maacs <- maacs[maacs$logpm25>0 & !is.na(maacs$logpm25),] 
+                        # I can kind of determine the first few and last bits 
+                        # of data. So I'm going to manully enter them since 
+                        # both sides didn't have many higher values, 
+                        # as such these data will simulate the original 
+                        # graphs a bit more accurately
+                        maacs <- arrange(maacs, logpm25)
+                        firstFew <- c(rep(0,7),2,0,3) # < 0.6
+                        lastBit <- c(2,0,0,2,0,0,3,3,0,4,4,4,3) # >2
+                        explicitNum <- sum(length(firstFew), length(lastBit))
+                        #I determined these values by comparing the visual 
+                        #on the graph and how the points were distributed, 
+                        #it seems some values of pm25 were changed from the plots 
+                        #shown, or the base of log wasn't 10
+                        
+                        #For the rest of the values I'm going to 
+                        #do my best to count how many I see on the graph then
+                        #randomly sample those values to generate NocturnalSympt
+                        freqs <- c(42,44,10,11,10,5,6,8,1,2,0,0,0,11)
+                        eyeCount <- c(rep(0, (length(maacs$logpm25) - sum(freqs, 
+                                                                          explicitNum))),
+                                      rep(1, freqs[1]), rep(2, freqs[2]), 
+                                      rep(3, freqs[3]), rep(4, freqs[4]),
+                                      rep(5, freqs[5]), rep(6, freqs[6]), 
+                                      rep(7, freqs[7]), rep(8, freqs[8]), 
+                                      rep(9, freqs[9]), rep(10, freqs[10]), 
+                                      rep(14, freqs[14]))
+                        
+                        sim <- sample(eyeCount, 
+                                      length(maacs$logpm25) - explicitNum)
+                        maacs <- mutate(maacs, 
+                                        NocturnalSympt = c(firstFew, sim, lastBit))
+                        
+                        #The final variable to simulate is bmicat, I'm going to use
+                        #The data from this
+                        #(https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1925022/)
+                        #study and say the random sample was 
+                        #representative of the population the study obtained 
+                        #a point estimate of 0.42% (25/60) overweight children.
+                        #I'll use this point estimate to create a 
+                        #random dist of overweight for maacs
+                        p <- 25/60
+                        m <- length(maacs$logpm25)*p
+                        
+                        randDist <- rnorm(length(maacs$logpm25), mean = m,
+                                          sd = sqrt((p*(1-p))/length(maacs$logpm25)))<m
+                        l <- c("overweight", "normal weight")
+                        maacs <- mutate(maacs, 
+                                        bmicat = factor(ifelse(randDist, l[1], l[2]),
+                                                        levels = c(l[1],l[2])))
+                        
+                        ## And that'll do it, now just to realign the data
+                        maacs <- arrange(maacs, id)
+                        return(maacs)}
+
+maacs2 <- bootstrapMissingVars()
+g <- ggplot(maacs2, aes(logpm25, NocturnalSympt)) #Creating the base
+summary(g) #Can look at summary of ggplot2
+print(g) #Nothing to print as only the base has been setup
+p <- g + geom_point() #Adding points to the based onject and assigning to a new variable
+print(p)
+
+g <- ggplot(maacs2, aes(logpm25, NocturnalSympt))
+g + geom_point() + geom_smooth()
+g + geom_point() + geom_smooth(method = "lm")
+g + geom_point() + geom_smooth(method = "lm") + facet_grid(.~bmicat) + labs(title = "Plot Title", subtitle = "Wow great subtitle", caption = "Caption 1", tag = "Tag!!")
+
+##Labels - xlab(), ylab(), labs(), ggtitle
+##Geom functions can be individually modified as required
+##Use global options to set general theme - use theme()
+
+g + geom_point(color = "steelblue", size = 4, alpha = 1/2) #Alpha is the transperancy #Assigning color to be a constant here
+g + geom_point(aes(color = bmicat), size =  4, alpha = 1/2) #Assigning color to be a variable using the aes function
+
+g + geom_point(aes(color = bmicat)) + labs(title = "MAACS Cohort") + labs(x = expression("log"*PM[2.5]), y = "Nocturnal Symptoms") + geom_smooth(size = 2, linetype = 4, method = "lm", se = FALSE)
+g + geom_point(aes(color = bmicat)) + theme_bw(base_family = "Times") #Applying a theme
+
+#Axis Limits
+
+testdat <- data.frame(x = 1:100, y = rnorm(100))
+testdat[50,2] <- 100 #Outlier
+plot(testdat$x, testdat$y, type = "l", ylim = c(-3,3))
+
+g <- ggplot(testdat, aes(x = x, y = y))
+g + geom_line()
+g + geom_line() + ylim(-3,3) #Not the right way as GGPLOT will just filter for values between -3 & 3
+g + geom_line() + coord_cartesian(ylim = c(-3,3)) #This is right way
+
+#Cutting a continous varibale to sets
+cutpoints <- quantile(maacs2$logpm25, seq(0,1, length = 4), na.rm = TRUE) #using quantile to find cutpoints
+maacs2$pmCat <- cut(maacs2$logpm25, cutpoints) #Cutting
+levels(maacs2$pmCat) #Checking the new variable
+
+g <- ggplot(maacs2, aes(logpm25, NocturnalSympt))
+g + 
+        geom_point(alpha = 1/2) + 
+        geom_smooth(method = "lm") + 
+        facet_grid(bmicat~pmCat) + 
+        labs(title = "MAACS Cohort", x = expression("log"*PM[2.5]), y = "Nocturnal Symptoms")
